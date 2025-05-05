4b4325081698d4a29758b3db2d32d724d3fda9bf
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-05-05 18:56:51 +0200

    Création de la fonction getCurrentYear qui va permettre l'affichage dynamique de l'année

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const menuBurger = document.querySelector(".header__burger__btn");
 //console.log(menuBurger);
 const navLinks = document.querySelectorAll(".header__link");
 //console.log(navLinks);
+const footerCopyrightYear = document.querySelector(".footer__text__year");
+//console.log(footerCopyrightYear);
 
 // Déclaration de la fonction toggleNav qui va permettre l'affichage des liens de navigation
 const toggleNav = () => {
@@ -24,3 +26,19 @@ navLinks.forEach((link) =>
     toggleNav();
   })
 );
+
+// Déclaration de la fonction getCurrentYear qui va permettre l'affichage dynamique de l'année
+const getCurrentYear = () => {
+  // Récupération de la date actuelle stockée dans la constante date
+  const date = new Date();
+  //console.log(date);
+
+  // Récupération de l'année stockée dans la constante year
+  const year = date.getFullYear();
+  //console.log(year);
+
+  // Affichage dynamique de l'année en cours
+  footerCopyrightYear.textContent = `${year}`;
+};
+// Appel de la fonction getCurrentYear()
+getCurrentYear();
