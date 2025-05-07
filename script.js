cdb6d7f80746ac75e1514bfbe03ed2454828779a
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-05-07 16:06:17 +0200

    Utilisation de l'API intersection Observer

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const firstName = document.querySelector("#firstName");
 const lastName = document.querySelector("#lastName");
 const email = document.querySelector("#email");
 const arrowBtn = document.querySelector(".arrow__btn");
+const cards = document.querySelectorAll(".card");
 
 // Déclaration de la fonction toggleNav qui va permettre l'affichage des liens de navigation
 const toggleNav = () => {
@@ -161,3 +162,28 @@ const scrollTo = () => {
 
 // Appel de la fonction scrollTo()
 scrollTo();
+
+// Création de l'objet options permettant de contrôler les circonstances selon lesquelles la fonction callback de l'observateur est invoquée
+let options = {
+  root: null, // l'élément qui est utilisé comme zone d'affichage au moment d'évaluer la visibilité de la cible. sa valeur par défaut (null) est la zone d'affichage (le viewport) du navigateur.
+  rootMargin: "-200px 0px", // La marge autour de la racine. Cet ensemble de valeur sert à agrandir ou à réduire chaque coté du cadre délimitant l'élément racine avant d'évaluer les intersections.
+  threshold: 0, // Indique à quel pourcentage de la visibilité de la cible la fonction callback de la cible doit être exécuté
+};
+
+// Déclaration de la fonction callback handleIntersect qui va permettre l'apparition des cartes
+const handleIntersect = (entries) => {
+  console.log(entries);
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+    }
+  });
+};
+
+// L'API Intersection Observer permet d'intégrer une fonction callback qui est exécutée quand un élément qu'on souhaite surveiller entre ou sort du viewport (zone d'affichage)
+const observer = new IntersectionObserver(handleIntersect, options);
+
+// Elément cible à observer
+cards.forEach((card) => {
+  observer.observe(card);
+});
